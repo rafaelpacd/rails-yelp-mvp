3642bbf303120e15c3e4e3eed9fcdbd46d022567
rafaelpando <rafaelpando9@gmail.com>
2022-08-24 16:49:17 +0100

    yelp

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,5 @@ class Review < ApplicationRecord
   validates :content, presence: true
   validates :rating, presence: true
   validates :rating, numericality: { only_integer: true }
-  validates :rating, inclusion: { in: (0..5) }
+  validates :rating, inclusion: { in: (0..5), allow_nil: false }
 end
